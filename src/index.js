b8ee992ac694b5562d75533470d7e313e760648c
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2024-03-13 05:01:53 +0000

    solution

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+root.render(
+   <>
+     <App />
+   </>
+);
